13387c2033683a26e6228826562764d215506f9f
Harish Kumar <harish.kumar@pkware.com>
2022-04-10 22:08:23 +0530

    implemented client side

---
+++ product-management-system/client-product-management/src/app/app.module.ts
@@ -0,0 +1,85 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { HomeComponent } from './components/user/home/home.component';
+import { DetailComponent } from './components/user/detail/detail.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { UserListComponent } from './components/admin/user-list/user-list.component';
+import { ProductListComponent } from './components/admin/product-list/product-list.component';
+import { UserTemplateComponent } from './components/template/user-template/user-template.component';
+import { AdminTemplateComponent } from './components/template/admin-template/admin-template.component';
+import { NotFoundComponent } from './components/error/not-found/not-found.component';
+import { UnathorizedComponent } from './components/error/unathorized/unathorized.component';
+
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatIconModule } from "@angular/material/icon";
+// import {
+//   MatButtonModule, MatCardModule,
+//   MatInputModule, MatListModule,
+//   MatToolbarModule, MatSelectModule,
+//   MatFormFieldModule, MatTableModule,
+//   MatPaginatorModule, MatSortModule,
+//   MatProgressBarModule, MatIconModule
+// } from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TransactionListComponent } from './components/admin/transaction-list/transaction-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    DetailComponent,
+    DashboardComponent,
+    UserListComponent,
+    ProductListComponent,
+    UserTemplateComponent,
+    AdminTemplateComponent,
+    NotFoundComponent,
+    UnathorizedComponent,
+    TransactionListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ product-management-system/client-product-management/src/app/components/admin/transaction-list/transaction-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {AdminService} from '../../../services/admin.service';
+import {Transaction} from '../../../model/transaction';
+
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatTableDataSource } from '@angular/material/table'
+import {MatSort, MatSortModule} from '@angular/material/sort';
+
+// import{MatPaginator, MatTableDataSource, MatSort} from '@angular/material';
+
+@Component({
+  selector: 'app-transaction-list',
+  templateUrl: './transaction-list.component.html',
+  styleUrls: ['./transaction-list.component.css']
+})
+export class TransactionListComponent implements OnInit {
+  transactionList: Array<Transaction>;
+  dataSource: MatTableDataSource<Transaction> = new MatTableDataSource();
+  displayedColumns: string[] = ['id', 'user', 'product'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit() {
+    this.findAllTransactions();
+  }
+
+  ngAfterViewInit(){
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  findAllTransactions(){
+    this.adminService.findAllTransactions().subscribe(data => {
+      this.transactionList = data;
+      this.dataSource = data;
+    });
+  }
+
+}
